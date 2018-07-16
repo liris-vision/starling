b72beeb949e5d2e9931e00d76de9e54e82fd7364
elombardi2 <elombardi2@nomail.com>
2018-07-16 13:17:11 +0200

    Fix issues on Ubuntu 18.04 due to GTK-Python wrapper (WIP)

--- harpia/s2iSessionManager.py
+++ harpia/s2iSessionManager.py
@@ -102,7 +102,7 @@ class s2iSessionManager:
 
 		# force events processing
 		while Gtk.events_pending():
-			Gtk.main_iteration_do()
+			Gtk.main_iteration_do(True)
 
 	def NewInstance(self , batchModeOn, a_lsXML = ["<harpia></harpia>"]):
 		self.StoreXML(a_lsXML)
