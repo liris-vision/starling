cbf16ab4bd4bf4b0f20427f2b204af2a016d133e
elombardi2 <elombardi2@nomail.com>
2018-07-25 14:11:26 +0200

    Remove splash screen

--- starling.py
+++ starling.py
@@ -54,114 +54,6 @@ from harpia import lvExtensions
 
 from time import sleep
 
-#TODO-fix-beg
-#class splashScreen():
-#	def __init__(self):
-#		self.window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
-#		
-#		self.window.set_type_hint(Gdk.WindowTypeHint.UTILITY)
-#		self.window.set_position(Gtk.WindowPosition.CENTER)
-#		self.window.set_decorated(0)
-#		self.window.set_default_size(960, 650)
-#		
-#		self.window.set_events(Gdk.EventMask.ALL_EVENTS_MASK)
-#		
-#		# Initialize colors, alpha transparency		
-#		self.window.set_app_paintable(1)
-#		self.gtk_screen = self.window.get_screen()
-#		colormap = self.gtk_screen.get_rgba_colormap()
-#		if colormap == None:
-#			colormap = self.gtk_screen.get_rgb_colormap()
-#		Gtk.widget_set_default_colormap(colormap)
-#		if not self.window.is_composited():
-#			self.supports_alpha = False
-#		else:
-#			self.supports_alpha = True
-#		
-#		self.w,self.h = self.window.get_size()
-#		
-#		self.window.connect("expose_event", self.expose)
-#		#self.window.connect("destroy", Gtk.main_quit)
-#		
-# 
-#	def expose (self, widget, event):
-#		self.ctx = self.window.window.cairo_create()
-#		# set a clip region for the expose event, XShape stuff
-#		self.ctx.save()
-#		if self.supports_alpha == False:
-#			self.ctx.set_source_rgb(1, 1, 1)
-#		else:
-#			self.ctx.set_source_rgba(1, 1, 1,0)
-#		self.ctx.set_operator (cairo.OPERATOR_SOURCE)
-#		self.ctx.paint()
-#		self.ctx.restore()
-#		self.ctx.rectangle(event.area.x, event.area.y,
-#				event.area.width, event.area.height)
-#		self.ctx.clip()
-#		self.draw_image(self.ctx,0,0,'base.png')
-# 
-#	def setup(self):
-#		# Set menu background image
-#		self.background =  Gtk.Image()
-#		self.frame = Gtk.Fixed()
-#		self.window.add (self.frame)
-#		
-#		# Set window shape from alpha mask of background image
-#		w,h = self.window.get_size()
-#		if w==0: w = 800
-#		if h==0: h = 650
-#		self.w = w
-#		self.h = h
-#		self.pixmap = Gtk.gdk.Pixmap (None, w, h, 1)
-#		ctx = self.pixmap.cairo_create()
-#		# todo: modify picture source
-#		#self.bgpb = GdkPixbuf.Pixbuf.new_from_file('base.png')
-#		ctx.save()
-#		ctx.set_source_rgba(1, 1, 1,0)
-#		ctx.set_operator (cairo.OPERATOR_SOURCE)
-#		ctx.paint()
-#		ctx.restore()
-#		self.draw_image(ctx,0,0,'base.png')
-#		
-#		if self.window.is_composited():
-#			ctx.rectangle(0,0,w,h)
-#			ctx.fill()			
-#			self.window.input_shape_combine_mask(self.pixmap,0,0)
-#		else:
-#			self.window.shape_combine_mask(self.pixmap, 0, 0)
-# 
-#	def draw_image(self,ctx,x,y, pix):
-#		"""Draws a picture from specified path with a certain width and height"""
-# 
-#		ctx.save()
-#		ctx.translate(x, y)	
-#		pixbuf = GdkPixbuf.Pixbuf.new_from_file(pix)
-#		format = cairo.FORMAT_RGB24
-#		if pixbuf.get_has_alpha():
-#			format = cairo.FORMAT_ARGB32
-#		#if Images.flip != None:
-#		#	pixbuf = pixbuf.flip(Images.flip)
-#	
-#		iw = pixbuf.get_width()
-#		ih = pixbuf.get_height()
-#		image = cairo.ImageSurface(format, iw, ih)
-#		image = ctx.set_source_pixbuf(pixbuf, 0, 0)
-#		
-#		ctx.paint()
-#		puxbuf = None
-#		image = None
-#		ctx.restore()
-#		ctx.clip()
-# 
-#	def show_window(self):
-#		self.window.show_all()
-#		while Gtk.events_pending():
-#			Gtk.main_iteration()
-#		self.window.present()
-#		self.window.grab_focus()
-#		self.p = 1
-#TODO-fix-end
-
 
 def main(argv):
 
@@ -200,12 +92,6 @@ def main(argv):
 	if args.config_file:
 		lvExtensions.setConfigurationFileName(os.path.abspath(args.config_file))
 	
-	#TODO-fix if not batchMode:
-	#TODO-fix 	m = splashScreen()
-	#TODO-fix 	m.setup()
-	#TODO-fix 	m.show_window()
-	#TODO-fix 	sleep ( 2 )
-
 	# fix user files path due to changing dir to Starling dir
 	userFiles = [os.path.join(startDir, fileName) for fileName in userFiles]
 
@@ -213,11 +99,8 @@ def main(argv):
 	HarpiaFrontend = harpiagcfrontend.S2iHarpiaFrontend(userFiles, batchMode, experimentalMode)
 	
 	if not batchMode:
-		#TODO-fix  m.window.set_transient_for(HarpiaFrontend.gtkTopWindow)
 		HarpiaFrontend.show( center=0 )
 		sleep ( 1 )
-		#TODO-fix  m.window.destroy ()
-		#splScr.window.destroy() 
 		Gtk.main()
 	
 	#----------------------------------------------------------------------
